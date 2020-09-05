4716df6d9237de8569f2a6910827668a27ae7c4e
Tom Crouch <t.crouch@outwood.com>
2020-09-05 08:59:30 +0100

    initial commit

---
+++ lib/outwood_labels.rb
@@ -0,0 +1,6 @@
+require "outwood_labels/version"
+
+module OutwoodLabels
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/outwood_labels/version.rb
@@ -0,0 +1,3 @@
+module OutwoodLabels
+  VERSION = "0.1.0"
+end
